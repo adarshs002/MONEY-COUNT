27aa684c2197d9760a73b042dbb34a61fddb4008
Akshay <akshayganesh@techis.io>
2022-03-23 05:06:55 +0530

    completed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,7 +15,7 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 import django_heroku
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,3 +163,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'config.pagination.CustomizePagination',
     'PAGE_SIZE': 10
 }
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
